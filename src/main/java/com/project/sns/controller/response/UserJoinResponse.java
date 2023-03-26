9fb967225f9f6079800f189ab9e6fe5dd6361834
MoonStar <moonsung0331@gmail.com>
2023-03-27 01:18:25 +0900

    #post : 포스트 작성 기능 구현 중 필요한 부가적인 부분 구현

--- src/main/java/com/project/sns/controller/response/UserJoinResponse.java
+++ src/main/java/com/project/sns/controller/response/UserJoinResponse.java
@@ -14,6 +14,6 @@ public class UserJoinResponse {
     private UserRole role;
 
     public static UserJoinResponse fromUser(User user) {
-        return new UserJoinResponse(user.getId(), user.getUserName(), user.getUserRole());
+        return new UserJoinResponse(user.getId(), user.getUsername(), user.getUserRole());
     }
 }
